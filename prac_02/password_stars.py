fcd5c77d810ec1b45f3826407316bec58ce92f66
Nathaniel-Carl-Peter <nathanielcarl.peter@my.jcu.edu.au>
2024-02-09 22:47:32 +1000

    Password stars version 2

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -4,15 +4,32 @@ Date: 09/02/24
 """
 
 """
-get user password
-display *
+def main
+    get user password
+    display *
 """
 
-# Version 1
 MINIMUM_LENGTH = 4
 
-user_password = input(f"Enter password with the minimum characters of {MINIMUM_LENGTH}: ")
-while len(user_password) < MINIMUM_LENGTH:
-    print("Invalid password")
+
+def main():
+    """Main Password Program"""
+    user_password = get_password()
+    print_stars(user_password)
+
+
+def print_stars(user_password):
+    """Print number *"""
+    print("*" * len(user_password))
+
+
+def get_password():
+    """Get password with error checking"""
     user_password = input(f"Enter password with the minimum characters of {MINIMUM_LENGTH}: ")
-print("*" * len(user_password))
+    while len(user_password) < MINIMUM_LENGTH:
+        print("Invalid password")
+        user_password = input(f"Enter password with the minimum characters of {MINIMUM_LENGTH}: ")
+    return user_password
+
+
+main()
