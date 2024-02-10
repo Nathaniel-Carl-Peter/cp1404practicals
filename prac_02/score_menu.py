07f93f966977d950ce5f631f701847557a2ff85e
Nathaniel-Carl-Peter <nathanielcarl.peter@my.jcu.edu.au>
2024-02-10 20:35:33 +1000

    score menu valid score: v2 score menu stars: work in progress

--- prac_02/score_menu.py
+++ prac_02/score_menu.py
@@ -8,7 +8,7 @@ Date: 10/02/2024
 # (S)how stars (this should print as many stars as the score)
 # (Q)uit
 
-MENU = "(V)alid score\n (S)tars\n (Q)uit"
+MENU = "(V)alid score\n(S)tars\n(Q)uit"
 
 
 def main():
@@ -17,13 +17,13 @@ def main():
     choice = input(">>> ").upper()
     while choice != "Q":
         if choice == "V":
-            score = float(input("Enter grade: "))
-            print(define_grade(score))
+            score = get_score()
+            grade = define_grade(score)
+            print(grade)
         elif choice == "S":
-            user_number = int(input(f"Enter number: "))
-            while len(user_number) < score:
-                print("Too low")
-                user_number = int(input(f"Enter number: "))
+            user_number = int(input(f"Enter valid number: "))
+            while user_number < score:
+                user_number = int(input(f"Enter valid number: "))
             print("*" * len(user_number))
         else:
             print("Invalid choice")
@@ -32,7 +32,14 @@ def main():
     print("Thank you")
 
 
+def get_score():
+    """Get score from the user"""
+    score = float(input("Enter grade: "))
+    return score
+
+
 def define_grade(score):
+    """Determine grade"""
     if score < 0 or score > 100:
         return "Invalid score"
     elif score >= 90:
