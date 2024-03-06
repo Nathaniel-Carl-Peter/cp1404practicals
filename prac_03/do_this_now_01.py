669235db94611105b968f7b143e87788fdcb02ff
Nathaniel-Carl-Peter <nathanielcarl.peter@my.jcu.edu.au>
2024-03-06 16:24:06 +1000

    Do this now base on video complete

--- prac_03/do_this_now_01.py
+++ prac_03/do_this_now_01.py
@@ -1,10 +1,10 @@
 FILENAME = "teststar.txt"
 
 # with method used
-with open(FILENAME, 'r') as in_file:
-    for line in in_file:
-        if line[0] == "#":
-            print(line)
+# with open(FILENAME, 'r') as in_file:
+#     for line in in_file:
+#         if line[0] == "#":
+#             print(line)
 
 # Without with method
 in_file = open(FILENAME, 'r')
