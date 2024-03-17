b278cda1c5d3ab79bffc807163d0717b5c02cc2f
Nathaniel-Carl-Peter <nathanielcarl.peter@my.jcu.edu.au>
2024-03-17 20:23:06 +1000

    Guitars finish v2

--- prac_06/guitar_test.py
+++ prac_06/guitar_test.py
@@ -7,9 +7,12 @@ def test():
     year = 1922
     cost = 16035.40
     guitar = Guitar(name, year, cost)
-    other = Guitar("Another Guitar", )
-    print(guitar)
-
+    other = Guitar("Another Guitar", 2013, 1500)
+    # print(guitar)
+    print(f"{guitar.name} get_age() - Expected {95}. Got {guitar.get_age()}")
+    print(f"{other.name} get_age() - Expected {9}. Got {other.get_age()}")
+    print(f"{guitar.name} is_vintage() - Expected {True}. Got {guitar.is_vintage()}")
+    print(f"{other.name} is_vintage() - Expected {False}. Got {other.is_vintage()}")
     print(f"My guitar: {name}, first made in {year} ${cost}")
 
 
