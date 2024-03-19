b7708c076b69ef827f8ca8a194b8785a161e100c
Nathaniel-Carl-Peter <nathanielcarl.peter@my.jcu.edu.au>
2024-03-19 17:56:06 +1000

    cars complete

--- prac_06/used_cars.py
+++ prac_06/used_cars.py
@@ -9,10 +9,14 @@ from prac_06.car import Car
 
 def main():
     """Demo test code to show how to use car class."""
+    # My car information
+
     my_car = Car("My car", 180)
     my_car.drive(30)
     print(f"fuel: {my_car.fuel}")
     print(my_car)
+
+    # Limo car information
 
     limo = Car("Limo", 100)
     limo.add_fuel(20)
