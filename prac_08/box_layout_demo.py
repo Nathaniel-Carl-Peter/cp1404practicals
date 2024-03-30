5f9614e58abaea218e73474e519adcc177b897f5
Nathaniel-Carl-Peter <nathanielcarl.peter@my.jcu.edu.au>
2024-03-30 22:54:32 +1000

    Add handle greet method

--- prac_08/box_layout_demo.py
+++ prac_08/box_layout_demo.py
@@ -1,3 +1,4 @@
+import random
 from kivy.app import App
 from kivy.lang import Builder
 
@@ -7,6 +8,12 @@ class BoxLayoutDemo(App):
         self.title = "Box Layout Demo"
         self.root = Builder.load_file('box_layout.kv')
         return self.root
+
+    def handle_greet(self):
+        if random.randint(1, 10) <= 5:
+            self.root.ids.my_label.text = "ouch!!"
+        else:
+            self.root.ids.my_label.text = "stop that!!"
 
 
 BoxLayoutDemo().run()
