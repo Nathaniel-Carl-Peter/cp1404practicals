a47526a37eb4c427da765fe042d03568d8aa55a6
Nathaniel-Carl-Peter <nathanielcarl.peter@my.jcu.edu.au>
2024-04-19 03:01:15 +1000

    creat a def__str method

--- prac_09/band.py
+++ prac_09/band.py
@@ -1,5 +1,4 @@
-from muscian import Musician
-from guitar import Guitar
+"""Cp1404 Band class"""
 
 
 class Band:
@@ -17,5 +16,9 @@ class Band:
         if not self.musicians:
             return f"{self.name} has no musicians."
         else:
-            musician_info = "\n".join(f"{m.name}: {m.play()}" for m in self.musicians)
+            musician_info = "\n".join(f"{muscian.name}: {muscian.play()}" for muscian in self.musicians)
             return f"{self.name} musicians:\n{musician_info}"
+
+    def __str__(self):
+        """Output as string representation for band"""
+        return f'{self.name} ({len(self.musicians)} musicians)'
