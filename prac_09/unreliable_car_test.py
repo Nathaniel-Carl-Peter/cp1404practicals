7ab0487ccfb25978f1bfa7da24a33c70580f666f
Nathaniel-Carl-Peter <nathanielcarl.peter@my.jcu.edu.au>
2024-04-19 00:39:53 +1000

    Little cleaning for readability

--- prac_09/unreliable_car_test.py
+++ prac_09/unreliable_car_test.py
@@ -10,12 +10,14 @@ def main():
     # Cars with different state of reliability status
     nice_car = UnreliableCar('Good Car', 100, 85)
     shady_car = UnreliableCar('Sussy Car', 100, 8)
+
     # Attempt to drive car a number of times
     for i in range(1, 12):
         print(f'Drive {i}km')
         print(f"{nice_car.name:12} drove {nice_car.drive(i):2}km")
         print(f"{shady_car.name:12} drove {shady_car.drive(i):2}km")
     print()
+
     # Print state of cars
     print(nice_car)
     print(shady_car)
